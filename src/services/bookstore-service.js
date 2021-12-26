97f9e55f8a75e8332bf10a88b529007b209c700d
Aleksandrgit config --global user.email  aleksrowing232@gmail.com <aleksrowing232@gmail.com>
2021-12-26 13:31:50 +0300

    нг

---
+++ src/services/bookstore-service.js
@@ -0,0 +1,17 @@
+
+export default class BookstoreService {
+
+  getBooks() {
+    return [
+      {
+        id: 1,
+        title: 'Идеальнный программист',
+        author: 'Роберт Мартин' },
+      {
+        id: 2,
+        title: 'Жизнь взаймы',
+        author: 'Эрих Мария Ремарк'}
+    ];
+  }
+
+}
